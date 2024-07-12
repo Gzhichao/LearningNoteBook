d19403e302d6b8624a48419be84f782e8ed93b18
Gzhichao <zhichaoguo75@gmail.com>
2024-07-12 15:42:20 +0800

    update

--- scripts/OrphanBlock.sh
+++ scripts/OrphanBlock.sh
@@ -19,15 +19,17 @@ do
     HashHeightParentsParentBlockHeight="$ScriptDir/HashHeightParentsParentBlockHeight"
     [ ! -f "/opt/raid0/lotusminer-winning/lotusminer/logs" ] && echo "日志文件不存在，请检查" && exit
     grep -ar 'mined new block.*{"cid":' $WinningLog |grep "${DateTime}T" > $MinedNewBlock
-    LotteryNum=$(grep -ar '"isEligible": true, "isWinner": false' $WinningLog |grep "${DateTime}T" -c)
-    WinNum=$(grep -ar '"isEligible": true, "isWinner": true'  $WinningLog |grep "${DateTime}T" -c)
-    MinedNewBlockNum=$(grep -c "${DateTime}T" $MinedNewBlock)
+    LotteryNum=$(grep -ar '"isEligible": true, "isWinner": false' $WinningLog |grep "${DateTime}T" -c) #抽奖数量
+    WinNum=$(grep -ar '"isEligible": true, "isWinner": true'  $WinningLog |grep "${DateTime}T" -c) #中奖数量
+    # 过滤区块哈希，区块高度，父区块到一个文件
     awk '{print $9,$11,$15}' $MinedNewBlock |sed 's#,$##' > $HashHeightParents
     > $WinParentBlockHeight
     for forRound in $(awk '{print $2}' $HashHeightParents |sed 's#,##')
     do
+	    # 过滤区块高度的父区块高度到一个文件
             grep -ar '"isEligible": true, "isWinner": true' $WinningLog |grep "${DateTime}T" |awk '/"forRound": '$forRound'/{print $12}' |sed 's#,##' >> $WinParentBlockHeight
     done
+    # 合并区块哈希，区块高度，父区块， 父区块高度到一个文件
     paste $HashHeightParents $WinParentBlockHeight > $HashHeightParentsParentBlockHeight
     python3 $ScriptDir/ComparedParents.py $ScriptDir $MinerID $DateTime $LotteryNum $WinNum
 done
